13974c2552b17ab7fd7ad8093a0eefe1034c3652
Naveen S <naveen2aug@gmail.com>
2024-04-07 14:48:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,9 @@
-import json
-import secret_info
-import mysql.connector
+import flask
 
-RemoteMysql = secret_info.RemoteMysql
+app = flask.Flask(__name__)
 
-mydb = mysql.connector.connect(host=RemoteMysql.host, user=RemoteMysql.user, passwd=RemoteMysql.passwd, database=RemoteMysql.database)
-mydbCursor = mydb.cursor()
-
-def lambda_handler(event, context):
-    publicIP=event["queryStringParameters"]["publicIP"]
-    sql = """UPDATE `EC2ServerPublicIP` SET %s = '%s' WHERE %s = %d""" % ("publicIP",publicIP,"ID", 1)
-    # ruleid: mysql-sqli
-    mydbCursor.execute(sql)
-
-    # ok: mysql-sqli
-    mydbCursor.execute("UPDATE `EC2ServerPublicIP` SET %s = '%s' WHERE %s = %s", ("publicIP",publicIP,"ID", 1))
-    mydb.commit()
-    
-    Body={
-        "publicIP":publicIP
-        
-    }
-    return {
-        'statusCode': 200,
-        'body': json.dumps(Body)
-    }
+@app.route("/route_param/<route_param>")
+def route_param(route_param):
+    print("blah")
+    # ruleid: eval-injection
+    return eval(route_param)
